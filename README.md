d077b311412700996bad4ff5881b2c1c1df038f1
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-03-09 08:19:43 -0800

    clear input on search and prevent blank searches

--- README.md
+++ README.md
@@ -8,9 +8,11 @@
 TODO
 
 DOING
-- make new search clear old results
-- add links to articles
 
 DONE
 - add button to navigate to a random wikipedia page
 - implement wikipedia api call
+- make new search clear old results
+- add links to articles
+- clear input on search
+- prevent blank searches
--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-$('#search').on('click', () => {
-  let terms = $('#query').val().split(' ').join('+');
-  $('.my-new-list').remove();
-  $.ajax({
-    url: 'https://en.wikipedia.org/w/api.php',
-    jsonp: 'callback',
-    dataType: 'jsonp',
-    data: {
-      action: 'query',
-      list: 'search',
-      srsearch: terms,
-      format: 'json'
-    },
-    xhrFields: { withCredentials: true },
-    success: (data) => {
-      let items = [];
-      $.each(data.query.search, (key, val) => {
-        const link = '<a href="http://en.wikipedia.org/wiki/' + val.title.replace(new RegExp(' ', 'g'), '_') + '" target="_blank">' + val.title + '</a>';
-        items.push('<li class="green"><h4>' + key + ': ' + link + '</h4><p>'+ val.snippet +'</p></li>');
-      });
-      $("<ul/>", {
-        "class": "my-new-list",
-        html: items.join("")
-      }).appendTo(".well");
-      console.log(data);
-    }
-  });
+$('#query').keypress( (e) => {
+  if(e.which == 13) {
+    $('#search').click();
+    return false;
+  }
 });
 
 
-function doSearch() {
-  alert("yo");
-}
+$('#search').on('click', () => {
+  const query = $('#query').val();
+
+  $('#query').val('');
+
+  if(query != '') {
+    $('.my-new-list').remove();
+    const terms = query.split(' ').join('+');
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php',
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      data: {
+        action: 'query',
+        list: 'search',
+        srsearch: terms,
+        format: 'json'
+      },
+      xhrFields: { withCredentials: true },
+      success: (data) => {
+        let items = [];
+        $.each(data.query.search, (key, val) => {
+          const link = '<a href="http://en.wikipedia.org/wiki/' + val.title.replace(new RegExp(' ', 'g'), '_') + '" target="_blank">' + val.title + '</a>';
+          items.push('<li class="green"><h4>' + key + ': ' + link + '</h4><p>'+ val.snippet +'</p></li>');
+        });
+        $("<ul/>", {
+          "class": "my-new-list",
+          html: items.join("")
+        }).appendTo(".well");
+        console.log(data);
+      }
+    });
+  }
+});
