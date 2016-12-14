c07ef9d11ec9f7f2b26264fa05a73d6c207e79b0
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-12-14 12:19:40 -0800

    Add search (Closes #8)

--- src/action_creators.js
+++ src/action_creators.js
@@ -5,11 +5,11 @@ export function receiveArticle(data) {
     };
 }
 
-const api = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=sparta&limit=10';
+const api = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&search=';
 const cors = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://gordon2012-cors-anywhere.herokuapp.com';
 
-export const fetchArticle = () => dispatch => {
-    return fetch(`${cors}/${api}`)
+export const fetchArticle = (search) => dispatch => {
+    if(search) return fetch(`${cors}/${api}${search}`)
         .then((res, rej) => res.json())
         .then(json => dispatch(receiveArticle(json)));
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,27 @@ import {connect} from 'react-redux';
 import * as actionCreators from '../action_creators';
 
 export default class App extends Component {
+
+  handleSearch(e) {
+    e.preventDefault();
+
+    const search = this.searchinput.value.trim();
+    if(search) this.props.fetchArticle(search);
+  }
+
   render() {
+    const {search, results} = this.props;
+
+    const resultsNode = results && results.length > 0 ?
+      results.map( (e, i) => <article key={i}><p><strong>{e.title}: </strong>{e.text}</p></article>)
+    :
+      <p>No results</p>
+
+    const searchNode = search ?
+      resultsNode
+    :
+      null;
+
     return (
       <div className="app">
         <header>
@@ -12,12 +32,9 @@ export default class App extends Component {
         <div className="content">
           <div className="wikiviewer">
             <button onClick={e => { window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank'); }}>Random</button>
-            <input type="text" />
-            <button onClick={this.props.fetchArticle}>Search</button>
-
-            { this.props.results && this.props.results.map( (e, i) =>
-                <article key={i}><p><strong>{e.title}: </strong>{e.text}</p></article>
-            )}
+            <input type="text" ref={(c) => this.searchinput = c} />
+            <button onClick={this.handleSearch.bind(this)}>Search</button>
+            {searchNode}
 
             {process.env.NODE_ENV === 'development' && <div>
               <hr /><pre><code>{JSON.stringify(this.props, null, 2)}</code></pre>
