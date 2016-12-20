7aaa284ce7d6edc379cf42e69eba3361d143036b
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-12-19 19:00:00 -0800

    Add links to wikipedia articles (Closes #14)

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 export default class Article extends Component {
     render() {
-        const {title, text} = this.props.data;
+        const {title, text, url} = this.props.data;
 
-        return <article>
+        return <article onClick={ e => { window.open(url, '_blank'); }}>
             <p>
                 <strong>{title}: </strong>{text}
             </p>
